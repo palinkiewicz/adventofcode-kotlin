b2446e16c77de65078d146b650b2e45d72ed1818
palinkiewicz <pogromca.ap@gmail.com>
2023-12-04 18:25:49 +0100

    Add 2023 Day 03 Part 2 solution

--- adventofcode2023/Day03.kt
+++ adventofcode2023/Day03.kt
@@ -66,9 +66,61 @@ object Day03 {
 
         println(sum)
     }
+
+    fun part2(inputs: List<String>) {
+        fun getAsteriskPositions(): List<Pair<Int, Int>> {
+            val asteriskPositions = mutableListOf<Pair<Int, Int>>()
+            for (y in inputs.indices) {
+                for (x in inputs[0].indices) {
+                    if (inputs[y][x] == '*') {
+                        asteriskPositions.add(Pair(x, y))
+                    }
+                }
+            }
+            return asteriskPositions
+        }
+
+        fun getPartNumber(x: Int, y: Int): Pair<Int, Int>? {
+            if (inputs[y][x].isDigit()) {
+                var number = inputs[y][x].toString()
+                var checkLeft = x - 1
+                var checkRight = x + 1
+
+                while (checkLeft >= 0 && inputs[y][checkLeft].isDigit()) {
+                    number = inputs[y][checkLeft] + number
+                    checkLeft--
+                }
+                while (checkRight < inputs[0].length && inputs[y][checkRight].isDigit()) {
+                    number += inputs[y][checkRight]
+                    checkRight++
+                }
+
+                return Pair(checkLeft * y, number.toInt())
+            }
+            return null
+        }
+
+        println(getAsteriskPositions().sumOf { // first: x, second: y
+            val partNumbers = mutableListOf<Pair<Int, Int>>()
+
+            for (i in -1..1) {
+                for (j in -1..1) {
+                    val number = getPartNumber(it.first + j, it.second + i)
+
+                    if (number != null && !partNumbers.contains(number)) {
+                        partNumbers.add(number)
+                    }
+                }
+            }
+
+            if (partNumbers.size == 2) partNumbers[0].second * partNumbers[1].second else 0
+        })
+    }
+
 }
 
 fun main() {
     val inputs = File("resources/adventofcode2023/Day03.txt").readLines()
     Day03.part1(inputs)
+    Day03.part2(inputs)
 }
