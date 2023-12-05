62aab8ad8214876ffd9a6590a2618a9461f59109
palinkiewicz <pogromca.ap@gmail.com>
2023-12-05 21:33:02 +0100

    Add 2023 Day 05 Part 2 solution

--- adventofcode2023/Day05.kt
+++ adventofcode2023/Day05.kt
@@ -22,9 +22,70 @@ object Day05 {
             number
         })
     }
+
+    fun part2(inputs: List<String>) {
+        fun getRangesMaps(): Array<MutableList<LongArray>> {
+            val rangesMaps = Array<MutableList<LongArray>>(7) { mutableListOf() }
+            var mapIndex = 0
+
+            for (i in inputs.indices.drop(2)) {
+                if (inputs[i] == "")
+                    mapIndex++
+                else if (inputs[i].split(" ").size == 3)
+                    rangesMaps[mapIndex].add(inputs[i].split(" ").map { it.toLong() }.toLongArray())
+            }
+
+            return rangesMaps
+        }
+
+        fun getSeedRanges(): List<Pair<Long, Long>> {
+            val rangesLine = inputs[0].split(" ").drop(1)
+            val ranges: MutableList<Pair<Long, Long>> = mutableListOf()
+
+            for (i in rangesLine.indices) {
+                if (i % 2 == 0) ranges.add(
+                    Pair(
+                        rangesLine[i].toLong(),
+                        rangesLine[i].toLong() + rangesLine[i + 1].toLong()
+                    )
+                )
+            }
+
+            return ranges
+        }
+
+        val rangesMaps = getRangesMaps()
+        val seedRanges = getSeedRanges()
+
+        fun getSeedFromLocation(location: Long): Long {
+            var number = location
+            var mapIndex = rangesMaps.size - 1
+
+            while (mapIndex >= 0) {
+                for (line in rangesMaps[mapIndex]) {
+                    if (line[0] <= number && line[0] + line[2] > number) {
+                        number = line[1] + number - line[0]
+                        break
+                    }
+                }
+                mapIndex--
+            }
+
+            return number
+        }
+
+        for (location in 0L..Long.MAX_VALUE) {
+            val number = getSeedFromLocation(location)
+            if (seedRanges.any { number >= it.first && number < it.second }) {
+                println(location)
+                break
+            }
+        }
+    }
 }
 
 fun main() {
     val inputs = File("resources/adventofcode2023/Day05.txt").readLines()
     Day05.part1(inputs)
+    Day05.part2(inputs)
 }
