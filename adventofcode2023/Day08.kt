c0981c42f7f9f1c32d2244d547470b01ef71e67c
palinkiewicz <pogromca.ap@gmail.com>
2023-12-08 21:29:42 +0100

    Reformat 2023 Day 08 solutions

--- adventofcode2023/Day08.kt
+++ adventofcode2023/Day08.kt
@@ -3,60 +3,45 @@ package adventofcode2023
 import java.io.File
 
 object Day08 {
-    fun part1(inputs: List<String>) {
-        val instructions = inputs[0]
-        val nodes = inputs.drop(2).associate {
-            val node = it.replace(Regex("[^A-Z ]"), "").replace(Regex("\\s+"), " ").split(" ")
-            node[0] to Pair(node[1], node[2])
-        }
-
+    private fun getSteps(
+        instructions: String,
+        nodes: Map<String, Pair<String, String>>,
+        location: String,
+        endsWith: String
+    ): Long {
+        var current = location
         var steps = 0
-        var i = 0
-        var location = "AAA"
 
-        while (location != "ZZZ") {
-            location = if (instructions[i] == 'L') nodes[location]!!.first else nodes[location]!!.second
-            if (i + 1 == instructions.length) i = 0 else  i++
+        while (!current.endsWith(endsWith)) {
+            current = if (instructions[steps % instructions.length] == 'L') nodes[current]!!.first else nodes[current]!!.second
             steps++
         }
 
-        println(steps)
+        return steps.toLong()
     }
 
-    fun part2(inputs: List<String>) {
-        val instructions = inputs[0]
-        val nodes = inputs.drop(2).associate {
-            val node = it.replace(Regex("[^A-Z ]"), "").replace(Regex("\\s+"), " ").split(" ")
-            node[0] to Pair(node[1], node[2])
+    private fun lcm(a: Long, b: Long): Long {
+        var ma = a
+        var mb = b
+        var remainder: Long
+
+        while (mb != 0L) {
+            remainder = ma % mb
+            ma = mb
+            mb = remainder
         }
 
-        fun lcm(a: Long, b: Long): Long {
-            var ma = a
-            var mb = b
-            var remainder: Long
+        return a * b / ma
+    }
 
-            while (mb != 0L) {
-                remainder = ma % mb
-                ma = mb
-                mb = remainder
-            }
+    fun part1(instructions: String, nodes: Map<String, Pair<String, String>>) {
+        println(getSteps(instructions, nodes, "AAA", "ZZZ"))
+    }
 
-            return a * b / ma
-        }
-
+    fun part2(instructions: String, nodes: Map<String, Pair<String, String>>) {
         println(
             nodes.filter { it.key.endsWith('A') }.map {
-                var steps = 0
-                var i = 0
-                var location = it.key
-
-                while (!location.endsWith('Z')) {
-                    location = if (instructions[i] == 'L') nodes[location]!!.first else nodes[location]!!.second
-                    if (i + 1 == instructions.length) i = 0 else  i++
-                    steps++
-                }
-
-                steps.toLong()
+                getSteps(instructions, nodes, it.key, "Z")
             }.reduce { acc, i -> lcm(acc, i) }
         )
     }
@@ -64,6 +49,12 @@ object Day08 {
 
 fun main() {
     val inputs = File("resources/adventofcode2023/Day08.txt").readLines()
-    Day08.part1(inputs)
-    Day08.part2(inputs)
+    val instructions = inputs[0]
+    val nodes = inputs.drop(2).associate {
+        val node = it.replace(Regex("[^A-Z ]"), "").replace(Regex("\\s+"), " ").split(" ")
+        node[0] to Pair(node[1], node[2])
+    }
+
+    Day08.part1(instructions, nodes)
+    Day08.part2(instructions, nodes)
 }
