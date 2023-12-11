ee7b4749b4ca13d1330244e28b1c56924152475c
palinkiewicz <pogromca.ap@gmail.com>
2023-12-11 17:15:40 +0100

    Add 2023 Day 11 Part 2 solution

--- adventofcode2023/Day11.kt
+++ adventofcode2023/Day11.kt
@@ -9,55 +9,55 @@ object Day11 {
 
     private val inputs = File("resources/adventofcode2023/Day11.txt").readLines()
 
-    private fun getExpandedUniverse(universe: List<String>): List<String> {
-        val expandedUniverse = universe.map { it.toMutableList() }.toMutableList()
+    private fun getEmptyRowsAndColumns(): Pair<List<Int>, List<Int>> {
+        val rows = mutableListOf<Int>()
+        val columns = mutableListOf<Int>()
 
-        // Add horizontal lines
-        var i = 0
-        while (i < expandedUniverse.size) {
-            if (expandedUniverse[i].count {it == EMPTY_CHAR} == expandedUniverse[i].size) {
-                expandedUniverse.add(i, MutableList(expandedUniverse[i].size) { EMPTY_CHAR })
-                i++
-            }
-            i++
-        }
+        for (i in inputs.indices)
+            if (inputs[i].count {it == EMPTY_CHAR} == inputs[i].length)
+                rows.add(i)
+        for (i in inputs[0].indices)
+            if (inputs.count { it[i] == EMPTY_CHAR } == inputs.size)
+                columns.add(i)
 
-        // Add vertical lines
-        i = 0
-        while (i < expandedUniverse[0].size) {
-            if (expandedUniverse.count { it[i] == EMPTY_CHAR } == expandedUniverse.count()) {
-                expandedUniverse.forEach { it.add(i, EMPTY_CHAR) }
-                i++
-            }
-            i++
-        }
-
-        return expandedUniverse.map { it.joinToString("") }
+        return Pair(rows, columns)
     }
 
-    private fun getGalaxiesPositions(universe: List<String>): List<Pair<Int, Int>> {
+    private fun getGalaxiesPositions(additionalEmpty: Int = 0): List<Pair<Int, Int>> {
+        val (emptyRows, emptyColumns) = getEmptyRowsAndColumns()
         val positions: MutableList<Pair<Int, Int>> = mutableListOf()
-        for (y in universe.indices)
-            for (x in universe[y].indices)
-                if (universe[y][x] == GALAXY_CHAR) positions.add(Pair(x, y))
+
+        for (y in inputs.indices)
+            for (x in inputs[y].indices)
+                if (inputs[y][x] == GALAXY_CHAR)
+                    positions.add(Pair(
+                        x + emptyColumns.count {it < x} * additionalEmpty,
+                        y + emptyRows.count {it < y} * additionalEmpty
+                    ))
+
         return positions
     }
 
     private fun getDistance(positionA: Pair<Int, Int>, positionB: Pair<Int, Int>): Int =
         abs(positionA.first - positionB.first) + abs(positionA.second - positionB.second)
 
-    fun part1() {
-        val galaxiesPositions = getGalaxiesPositions(getExpandedUniverse(inputs))
-        var distancesSum = 0
+    private fun getDistancesSum(additionalEmpty: Int): Long {
+        val galaxiesPositions = getGalaxiesPositions(additionalEmpty)
+        var distancesSum = 0L
 
         for (i in galaxiesPositions.indices)
             for (j in i + 1..<galaxiesPositions.size)
                 distancesSum += getDistance(galaxiesPositions[i], galaxiesPositions[j])
 
-        println(distancesSum)
+        return distancesSum
     }
+
+    fun part1() = println(getDistancesSum(1))
+
+    fun part2() = println(getDistancesSum(999999))
 }
 
 fun main() {
     Day11.part1()
+    Day11.part2()
 }
