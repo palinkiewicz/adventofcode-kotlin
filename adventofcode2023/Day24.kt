b3cb5a2e6756de8deddaed89d825f68cd08489c9
palinkiewicz <pogromca.ap@gmail.com>
2023-12-26 15:26:22 +0100

    Create 2023 Day 24 solution file

---
+++ adventofcode2023/Day24.kt
@@ -0,0 +1,13 @@
+package adventofcode2023
+
+import java.io.File
+
+object Day24 {
+    private var inputs = File("resources/adventofcode2023/Day24.txt").readLines()
+
+    fun part1() = println()
+}
+
+fun main() {
+    Day24.part1()
+}
